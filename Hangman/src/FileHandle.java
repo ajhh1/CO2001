cd0d2c28f272770e14147360c41b987ea05f27d3
ajhh1 <ajhh1@student.le.ac.uk>
2016-11-22 13:53:59 +0000

    Changes

--- Hangman/src/FileHandle.java
+++ Hangman/src/FileHandle.java
@@ -1,9 +1,116 @@
+/*
+import java.io.BufferedReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FileHandle implements Runnable{ 
+	//static List<String>[] words = new ArrayList[4];
+	int file;
+	String[] inFiles = new String[3];
+	
+	//static List<List<String>> words = new ArrayList<List<String>>(4);
+	public static List<String>[] words = new List[4];
+	
+	public FileHandle(String[] inF, int f)
+	{
+		inFiles = inF;
+		file = f;
+	}
+	
+	public void fillInFiles()
+	{
+		for(int x =1; x<=inFiles.length;x++)
+		{
+			inFiles[x-1] = "file" + x + ".txt";
+		}
+	}
+	
+	@Override
+	public void run() {
+
+		Random rand = new Random();
+		
+		file = 0;
+		String word;
+		ArrayList<String> fileWords = new ArrayList<String>();
+		ArrayList<String> fiftyWords = new ArrayList<String>();
+		Path path = FileSystems.getDefault().getPath(inFiles[file]);
+					
+		try(BufferedReader rd = Files.newBufferedReader(path))
+		{
+			while((word = rd.readLine() ) != null)
+			{
+				fileWords.add(word);
+			}
+			
+				for (int i=0; i<50; i++)
+				{
+					int temp = rand.nextInt(fileWords.size());
+					fiftyWords.add(fileWords.get(temp));
+				}
+				
+			words[file] = fiftyWords;
+			file++;
+			System.out.println(fiftyWords);
+		}
+		
+		catch(Exception e){
+			System.out.println("Error");
+		}
+		
+		
+	}
+	
+	public void save50(int x) {
+
+		Random rand = new Random();
+		
+		file = 0;
+		String word;
+		ArrayList<String> fileWords = new ArrayList<String>();
+		ArrayList<String> fiftyWords = new ArrayList<String>();
+		Path path = FileSystems.getDefault().getPath(inFiles[x]);
+					
+		try(BufferedReader rd = Files.newBufferedReader(path))
+		{
+			while((word = rd.readLine() ) != null)
+			{
+				fileWords.add(word);
+			}
+			
+				for (int i=0; i<50; i++)
+				{
+					int temp = rand.nextInt(fileWords.size());
+					fiftyWords.add(fileWords.get(temp));
+				}
+				
+			words[file] = fiftyWords;
+			file++;
+		}
+		
+		catch(Exception e){
+			System.out.println("Error");
+		}
+		
+		
+	}
+
+
+
+	
+	}*/
+
 
 import java.io.BufferedReader;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -90,6 +197,7 @@ public class FileHandle implements Runnable{
 				
 			words[file] = fiftyWords;
 			file++;
+			System.out.println(fiftyWords);
 		}
 		
 		catch(Exception e){
@@ -103,4 +211,3 @@ public class FileHandle implements Runnable{
 
 	
 	}
-
--- Hangman/src/Threading.java
+++ Hangman/src/Threading.java
@@ -1,3 +1,111 @@
+/*import java.util.Arrays;
+import java.util.Scanner;
+
+public class Threading {
+	
+	public static int f;
+	public String file;
+	public static String[] files = new String[4];
+	static FileHandle fh = new FileHandle(files, f);
+	static Thread[] threads = new Thread[fh.inFiles.length];
+	static boolean running = true;
+	
+		
+	public static void main(String[]args) 
+	{
+		Scanner input = new Scanner(System.in);
+		fh.fillInFiles();
+		System.out.println(Arrays.asList(fh.inFiles));
+		
+		while(running)
+		{
+			System.out.println("Would you like to run the application parallel with multiple threads or sequentially with one thread?");
+			System.out.println("Please select P/S: ");
+			String temp = input.next().toUpperCase();		
+			
+			if(temp.equals("P"))
+			{
+				long pStartTime = System.currentTimeMillis();
+				
+				for(int i = 0; i<threads.length;i++)
+				{
+					threads[i] = new Thread(fh);
+					threads[i].start();
+				}
+				
+				for(Thread thread:threads)
+				{
+					try {
+						thread.join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				long pEndTime = System.currentTimeMillis();
+				
+				for(int i = 0; i<threads.length;i++)
+				{
+					fh.save50(i);
+				}
+				
+				long sEndTime = System.currentTimeMillis();
+				
+				long pTotalTime = pEndTime - pStartTime;
+				long sTotalTime = sEndTime - pEndTime;
+				
+				
+				System.out.println("The program took "+pTotalTime+" milliseconds to run.");
+				System.out.println("Alternatively, it would have taken "+sTotalTime+" milliseconds to run sequentially.");
+				running = false;
+			}
+			
+			else if(temp.equals("S"))
+			{
+				long sStartTime = System.currentTimeMillis();
+				
+				for(int i = 0; i<threads.length;i++)
+				{
+					fh.save50(i);
+				}
+				
+				long sEndTime = System.currentTimeMillis();
+				
+				for(int i = 0; i<threads.length;i++)
+				{
+					threads[i] = new Thread(fh);
+					threads[i].start();
+				}
+				
+				for(Thread thread: threads )
+				{
+					try {
+						thread.join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				long pEndTime = System.currentTimeMillis();
+				
+				long sTotalTime = sEndTime - sStartTime;
+				long pTotalTime = pEndTime - sEndTime;
+				System.out.println(Arrays.asList(fh.words));
+				System.out.println("The program took "+sTotalTime+" milliseconds to run.");
+				System.out.println("Alternatively, it would have taken "+pTotalTime+" milliseconds to run in parallel.");
+				
+				running = false;
+			}
+			
+			else{
+				System.out.println("Incorrect input, command not registered. Please retry: \n");
+			}
+		
+		}
+		
+	}
+
+}*/
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -49,7 +157,7 @@ public class Threading {
 				long pTotalTime = pEndTime - pStartTime;
 				long sTotalTime = sEndTime - pEndTime;
 				
-				System.out.println(Arrays.asList(fh.words));
+				//System.out.println(Arrays.asList(fh.words));
 				System.out.println("The program took "+pTotalTime+" milliseconds to run.");
 				System.out.println("Alternatively, it would have taken "+sTotalTime+" milliseconds to run sequentially.");
 				running = false;
